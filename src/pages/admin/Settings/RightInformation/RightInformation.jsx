305aef3a8653736304026692bbbfcb4abf6a3c0a
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2020-11-25 11:10:05 +0500

    add
    add

--- src/pages/admin/Settings/RightInformation/RightInformation.jsx
+++ src/pages/admin/Settings/RightInformation/RightInformation.jsx
@@ -11,28 +11,11 @@ import {
   Row,
   Col,
   CustomInput,
-  FormFeedback,
-  FormText,
+  // FormFeedback,
+  // FormText,
 } from "reactstrap";
 
 // const { Dragger } = Upload;
-
-// const props = {
-//   name: "file",
-//   multiple: true,
-//   action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-//   onChange(info) {
-//     const { status } = info.file;
-//     if (status !== "uploading") {
-//       console.log(info.file, info.fileList);
-//     }
-//     if (status === "done") {
-//       message.success(`${info.file.name} file uploaded successfully.`);
-//     } else if (status === "error") {
-//       message.error(`${info.file.name} file upload failed.`);
-//     }
-//   },
-// };
 
 const initialValues = {
   ism: "",
@@ -82,181 +65,206 @@ function RightInformation() {
           isSubmitting,
           setFieldValue,
           /* and other goodies */
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <Row>
-              <Col sm="12" md="6" className="mb-3">
-                <FormGroup>
-                  <Label for="ism">Ism</Label>
-                  <Input
-                    type="text"
-                    name="ism"
-                    id="ism"
-                    placeholder="Ismingizni kiriting"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.ism}
-                  />
+        }) => {
+          // const props = {
+          //   name: "file",
+          //   multiple: true,
+          //   action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+          //   onChange(info) {
+          //     const { status } = info.file;
+          //     if (status !== "uploading") {
+          //       console.log(info.file.response);
+          //     }
+          //     if (status === "done") {
+          //       message.success(
+          //         `${info.file.name} file uploaded successfully.`
+          //       );
+          //     } else if (status === "error") {
+          //       message.error(`${info.file.name} file upload failed.`);
+          //     }
+          //   },
+          // };
 
-                  {errors.ism && touched.ism ? (
-                    <p className="font-weight-bold text-danger">{errors.ism}</p>
-                  ) : null}
-                </FormGroup>
-              </Col>
-              <Col sm="12" md="6" className="mb-3">
+          return (
+            <Form onSubmit={handleSubmit}>
+              <Row>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label for="ism">Ism</Label>
+                    <Input
+                      type="text"
+                      name="ism"
+                      id="ism"
+                      placeholder="Ismingizni kiriting"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.ism}
+                    />
+
+                    {errors.ism && touched.ism ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.ism}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label for="familiya">Familiya</Label>
+                    <Input
+                      type="text"
+                      name="familiya"
+                      id="familiya"
+                      placeholder="Familiyangizni kiriting"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.familiya}
+                    />
+                    {errors.familiya && touched.familiya ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.familiya}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label for="link">Link</Label>
+                    <Input
+                      type="text"
+                      name="link"
+                      id="link"
+                      placeholder="Link kiriting"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.link}
+                    />
+                    {errors.link && touched.link ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.link}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label for="telegram">Telegram</Label>
+                    <Input
+                      type="text"
+                      name="telegram"
+                      id="telegram"
+                      placeholder="Telegram"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.telegram}
+                    />
+                    {errors.telegram && touched.telegram ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.telegram}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label
+                      for="phonenumber"
+                      className="font-weight-bold text-success"
+                    >
+                      phonenumber
+                    </Label>
+                    <Input
+                      className="is-valid"
+                      type="text"
+                      name="phonenumber"
+                      id="phonenumber"
+                      placeholder="Phonenumber"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.phonenumber}
+                    />
+
+                    {errors.phonenumber && touched.phonenumber ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.phonenumber}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+                <Col sm="12" md="6" className="mb-3">
+                  <FormGroup>
+                    <Label
+                      for="abouteme"
+                      className="text-danger font-weight-bold"
+                    >
+                      abouteme
+                    </Label>
+                    <Input
+                      className="is-invalid"
+                      type="textarea"
+                      name="abouteme"
+                      id="abouteme"
+                      placeholder="abouteme"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.abouteme}
+                    />
+                    {errors.abouteme && touched.abouteme ? (
+                      <p className="font-weight-bold text-danger">
+                        {errors.abouteme}
+                      </p>
+                    ) : null}
+                  </FormGroup>
+                </Col>
+              </Row>
+
+              <div className="mb-3">
+                {/* <Dragger {...props} listType="picture">
+                  <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p className="ant-upload-text">
+                    Click or drag file to this area to upload
+                  </p>
+                  <p className="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from
+                    uploading company data or other band files
+                  </p>
+                </Dragger> */}
+
                 <FormGroup>
-                  <Label for="familiya">Familiya</Label>
-                  <Input
-                    type="text"
-                    name="familiya"
-                    id="familiya"
-                    placeholder="Familiyangizni kiriting"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.familiya}
+                  <Label for="image">File Browser with Custom Label</Label>
+                  <CustomInput
+                    type="file"
+                    id="image"
+                    name="image"
+                    label="Yo, pick a file!"
+                    onChange={(event) => {
+                      setFieldValue("image", event.currentTarget.files[0]);
+                    }}
                   />
-                  {errors.familiya && touched.familiya ? (
+                  {errors.image && touched.image ? (
                     <p className="font-weight-bold text-danger">
-                      {errors.familiya}
+                      {errors.image}
                     </p>
                   ) : null}
                 </FormGroup>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm="12" md="6" className="mb-3">
-                <FormGroup>
-                  <Label for="link">Link</Label>
-                  <Input
-                    type="text"
-                    name="link"
-                    id="link"
-                    placeholder="Link kiriting"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.link}
-                  />
-                  {errors.link && touched.link ? (
-                    <p className="font-weight-bold text-danger">
-                      {errors.link}
-                    </p>
-                  ) : null}
-                </FormGroup>
-              </Col>
-              <Col sm="12" md="6" className="mb-3">
-                <FormGroup>
-                  <Label for="telegram">Telegram</Label>
-                  <Input
-                    type="text"
-                    name="telegram"
-                    id="telegram"
-                    placeholder="Telegram"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.telegram}
-                  />
-                  {errors.telegram && touched.telegram ? (
-                    <p className="font-weight-bold text-danger">
-                      {errors.telegram}
-                    </p>
-                  ) : null}
-                </FormGroup>
-              </Col>
-            </Row>
-            <Row>
-              <Col sm="12" md="6" className="mb-3">
-                <FormGroup>
-                  <Label
-                    for="phonenumber"
-                    className="font-weight-bold text-success"
-                  >
-                    phonenumber
-                  </Label>
-                  <Input
-                    className="is-valid"
-                    type="text"
-                    name="phonenumber"
-                    id="phonenumber"
-                    placeholder="Phonenumber"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.phonenumber}
-                  />
+              </div>
 
-                  {errors.phonenumber && touched.phonenumber ? (
-                    <p className="font-weight-bold text-danger">
-                      {errors.phonenumber}
-                    </p>
-                  ) : null}
-                </FormGroup>
-              </Col>
-              <Col sm="12" md="6" className="mb-3">
-                <FormGroup>
-                  <Label
-                    for="abouteme"
-                    className="text-danger font-weight-bold"
-                  >
-                    abouteme
-                  </Label>
-                  <Input
-                    className="is-invalid"
-                    type="textarea"
-                    name="abouteme"
-                    id="abouteme"
-                    placeholder="abouteme"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.abouteme}
-                  />
-                  {errors.abouteme && touched.abouteme ? (
-                    <p className="font-weight-bold text-danger">
-                      {errors.abouteme}
-                    </p>
-                  ) : null}
-                </FormGroup>
-              </Col>
-            </Row>
-
-            <div className="mb-3">
-              {/* <Dragger {...props} listType="picture">
-                <p className="ant-upload-drag-icon">
-                  <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">
-                  Click or drag file to this area to upload
-                </p>
-                <p className="ant-upload-hint">
-                  Support for a single or bulk upload. Strictly prohibit from
-                  uploading company data or other band files
-                </p>
-              </Dragger> */}
-
-              <FormGroup>
-                <Label for="image">File Browser with Custom Label</Label>
-                <CustomInput
-                  type="file"
-                  id="image"
-                  name="image"
-                  label="Yo, pick a file!"
-                  onChange={(event) => {
-                    setFieldValue("image", event.currentTarget.files[0]);
-                  }}
-                />
-                {errors.image && touched.image ? (
-                  <p className="font-weight-bold text-danger">{errors.image}</p>
-                ) : null}
-              </FormGroup>
-            </div>
-
-            <button
-              type="submit"
-              className="btn btn-info"
-              disabled={isSubmitting}
-            >
-              Submit
-            </button>
-          </Form>
-        )}
+              <button
+                type="submit"
+                className="btn btn-info"
+                disabled={isSubmitting}
+              >
+                Submit
+              </button>
+            </Form>
+          );
+        }}
       </Formik>
     </Card>
   );
