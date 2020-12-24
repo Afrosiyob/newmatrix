0d6f1b61614c2b57d2fdde62e36fb1f65503f41d
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2020-12-24 11:43:21 +0500

    addd
    addd

--- src/i18n.js
+++ src/i18n.js
@@ -24,6 +24,43 @@ i18n
     fallbackLng: "en",
     debug: true,
     whitelist: Languages,
+    detection: {
+      // order and from where user language should be detected
+      order: [
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+
+      // keys or params to lookup language from
+      lookupQuerystring: "lng",
+      lookupCookie: "i18next",
+      lookupLocalStorage: "i18nextLng",
+      lookupSessionStorage: "i18nextLng",
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+
+      // cache user language on
+      caches: ["localStorage", "cookie"],
+      excludeCacheFor: ["cimode"], // languages to not persist (cookie, localStorage)
+
+      // optional expire and domain for set cookie
+      cookieMinutes: 10,
+      cookieDomain: "myDomain",
+
+      // optional htmlTag with lang attribute, the default is:
+      htmlTag: document.documentElement,
+
+      // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+      cookieOptions: { path: "/", sameSite: "strict" },
+      // only detect languages that are in the whitelist
+      checkWhitelist: true,
+    },
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
